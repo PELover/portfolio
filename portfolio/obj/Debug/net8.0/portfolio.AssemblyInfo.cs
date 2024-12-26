9aa8ca320a3b78c0aabc94e9e9053a05fff81282
Nathaphong Prasertrat <mothzabot6@gmail.com>
2024-12-26 21:36:53 +0700

    save

--- portfolio/obj/Debug/net8.0/portfolio.AssemblyInfo.cs
+++ portfolio/obj/Debug/net8.0/portfolio.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("portfolio")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7aedd337f53560f50739bf48c2968ceab0d2fe11")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c3afc8810e90711bc65880424b0198b56346f574")]
 [assembly: System.Reflection.AssemblyProductAttribute("portfolio")]
 [assembly: System.Reflection.AssemblyTitleAttribute("portfolio")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
